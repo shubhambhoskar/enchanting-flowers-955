841b8ba37e5364e684b8e40dbd369428fe8233ce
Amit-singh-05 <ami9sin05@gmail.com>
2022-12-16 11:12:16 +0530

    update

---
+++ Shopping_Application/src/main/java/com/project/Dto/CustomerDto.java
@@ -0,0 +1,12 @@
+package com.project.Dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDto {
+	private String userName;
+	private String password;
+}
