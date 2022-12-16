749c417d6d2e3d5010ae0b48c83f61a6fb434d1f
Shashi Suman <shashiaryavanshi145@gmail.com>
2022-12-16 16:35:51 +0530

    Delete ShoppingApp directory

--- ShoppingApp/src/main/java/com/project/Dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.project.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.module.User;
-
-public interface UserDao  extends JpaRepository<User, Integer>{
-
-}
--- ShoppingApp/src/main/java/com/project/Service/ProductService.java
+++
@@ -1,5 +0,0 @@
-package com.project.Service;
-
-public interface ProductService {
-
-}
